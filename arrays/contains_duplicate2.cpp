2954980128c20623f52481d57fdd357089896c93
Alok Kumar Mishra <alokkumar_mishra@epam.com>
2026-04-04 00:58:10 +0530

    Added solution of Contains Duplicate II

---
+++ arrays/contains_duplicate2.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+    
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        int n = nums.size();
+        unordered_map<int, int> mp;
+
+        for(int i=0;i<n;i++)
+        {
+            if(mp.count(nums[i]) && abs(i - mp[nums[i]]) <= k)
+            {
+                return true;
+            }
+            mp[nums[i]] = i;
+        }
+        return false;
+    }
