bcaa9f352b5b8e2869de35a2c5d2f2e952f61ced
Alok Mishra <alokbuntymishra@gmail.com>
2026-04-11 19:18:14 +0530

    Added solution for reverse bits

---
+++ Bit_Manipulation/reversebits.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int reverseBits(int n)
+{
+    int result;
+    for (int i = 0; i < 32; i++)
+    {
+        result = result << 1;
+        result = result | (n & 1);
+        n = n >> 1;
+    }
+
+    return result;
+}
