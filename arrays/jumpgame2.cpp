03a8956fcd3bdd30cab34c9af6ee7fc86ed7c7a0
Alok Mishra <alokbuntymishra@gmail.com>
2026-04-19 14:41:46 +0530

    Added solution of Jump game 2 from leetcode

---
+++ arrays/jumpgame2.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+int jump(vector<int>& nums) {
+        int jumps = 0;
+        int l = 0, r = 0;
+
+        while (r < nums.size() - 1) {
+            int farthest = 0;
+            for (int i = l; i <= r; i++) {
+                farthest = max(farthest, (i + nums[i]));
+            }
+            l = r + 1;
+
+            r = farthest;
+            jumps = jumps + 1;
+        }
+        return jumps;
+    }
