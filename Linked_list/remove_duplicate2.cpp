6f9e9c925f2e15bf9388d15375263fb80ff41fc0
Alok Mishra <alokbuntymishra@gmail.com>
2026-04-24 21:12:16 +0530

    Added solution of remove duplicates 2

---
+++ Linked_list/remove_duplicate2.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+ListNode* deleteDuplicates(ListNode* head) {
+        ListNode* curr = head;
+        ListNode* dummy = new ListNode(0);
+        dummy->next = head; 
+        ListNode* prev = dummy;
+        
+        while(curr != NULL)
+        {
+            if(curr->next != NULL && curr->val == curr->next->val)
+            {
+                while(curr->next != NULL && curr->val == curr->next->val)
+                {
+                    curr = curr->next;
+                }
+                prev->next = curr->next;
+            }
+            else
+            {
+                prev = prev->next;
+            }
+            curr = curr->next;
+        }
+        return dummy->next;
+    }
