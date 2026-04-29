b2f8b2856db49127272044a3f76d531672da8747
Alok Mishra <alokbuntymishra@gmail.com>
2026-04-29 23:44:03 +0530

    Added solution of Number 1 Bits

---
+++ Bit_Manipulation/Number1Bits.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int hammingWeight(int n) {
+        int cnt = 0;
+        while(n > 0)
+        {
+            n = n & (n-1);
+            cnt++;
+        }
+        return cnt;
+    }
