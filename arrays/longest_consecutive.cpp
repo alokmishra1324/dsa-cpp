efe3aa28c426f02e0ee11a055b55d9b2fbba3770
Alok Mishra <alokbuntymishra@gmail.com>
2026-05-03 20:26:11 +0530

    Added solution of Longest Consecutive Sequence

---
+++ arrays/longest_consecutive.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int longestConsecutive(vector<int>& nums) {
+        unordered_set<int> s(nums.begin(), nums.end());
+
+        int longest = 0;
+        for (int num : s) {
+            if (s.find(num - 1) == s.end()) {
+                int curr = num;
+                int count = 1;
+                while (s.find(curr + 1) != s.end()) {
+                    curr++;
+                    count++;
+                }
+
+                longest = max(longest, count);
+            }
+        }
+        return longest;
+    }
