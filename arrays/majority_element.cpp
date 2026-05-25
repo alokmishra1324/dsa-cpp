f103d9c598a5721d378564cf31611074362a7f93
Alok Mishra <alokbuntymishra@gmail.com>
2026-05-25 23:16:33 +0530

    Added solution of majority element

---
+++ arrays/majority_element.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int majorityElement(vector<int>& nums) {
+        int n = nums.size();
+        int majority = nums[0];
+        int votes = 1;
+        for (int i = 1; i < n; i++) {
+            if (votes == 0) {
+                votes++;
+                majority = nums[i];
+            } else if (majority == nums[i]) {
+                votes++;
+            } else {
+                votes--;
+            }
+        }
+        return majority;
+    }
